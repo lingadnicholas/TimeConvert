bf001318dff5984ffe219339e6b358080b10f5be
Nicholas Lingad <lingadnicholas@engineering.ucla.edu>
2023-06-13 18:32:48 -0700

    Added functionality to set timezone

--- script.js
+++ script.js
@@ -73,4 +73,17 @@ function updateNumber (element, number) {
   }, 990)
 }
 
+function setTimezone() {
+    // https://stackoverflow.com/questions/9149556/how-to-get-utc-offset-in-javascript-analog-of-timezoneinfo-getutcoffset-in-c
+    var d = new Date()
+    var utcHours = -d.getTimezoneOffset()/60;
+    var tz_offset = document.querySelector(".tz_offset")
+    tz_offset.innerHTML = "UTC" + utcHours; 
+
+
+    var tz_identifier = document.querySelector(".tz_identifier")
+    tz_identifier.innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone
+}
+
+setTimezone();
 setInterval(updateTime, 100)
