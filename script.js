80b1bb7a40a545603d735c48ca7800e21b3e1808
Nicholas Lingad <lingadnicholas@engineering.ucla.edu>
2023-06-13 19:11:15 -0700

    Removed clock functionality

--- script.js
+++ script.js
@@ -1,77 +1,4 @@
-// updateTime, tick, updateContainer, updateNumber, from: https://codepen.io/dervondenbergen/pen/dVPOwB 
-var hoursContainer = document.querySelector('.hours')
-var minutesContainer = document.querySelector('.minutes')
-var secondsContainer = document.querySelector('.seconds')
-var tickElements = Array.from(document.querySelectorAll('.tick'))
 
-var last = new Date(0)
-last.setUTCHours(-1)
-
-var tickState = true
-
-function updateTime () {
-  var now = new Date
-  
-  var lastHours = last.getHours().toString()
-  var nowHours = now.getHours().toString()
-  if (lastHours !== nowHours) {
-    updateContainer(hoursContainer, nowHours)
-  }
-  
-  var lastMinutes = last.getMinutes().toString()
-  var nowMinutes = now.getMinutes().toString()
-  if (lastMinutes !== nowMinutes) {
-    updateContainer(minutesContainer, nowMinutes)
-  }
-  
-  var lastSeconds = last.getSeconds().toString()
-  var nowSeconds = now.getSeconds().toString()
-  if (lastSeconds !== nowSeconds) {
-    //tick()
-    updateContainer(secondsContainer, nowSeconds)
-  }
-  
-  last = now
-}
-
-function tick () {
-  tickElements.forEach(t => t.classList.toggle('tick-hidden'))
-}
-
-function updateContainer (container, newTime) {
-  var time = newTime.split('')
-  
-  if (time.length === 1) {
-    time.unshift('0')
-  }
-  
-  
-  var first = container.firstElementChild
-  if (first.lastElementChild.textContent !== time[0]) {
-    updateNumber(first, time[0])
-  }
-  
-  var last = container.lastElementChild
-  if (last.lastElementChild.textContent !== time[1]) {
-    updateNumber(last, time[1])
-  }
-}
-
-function updateNumber (element, number) {
-  //element.lastElementChild.textContent = number
-  var second = element.lastElementChild.cloneNode(true)
-  second.textContent = number
-  
-  element.appendChild(second)
-  element.classList.add('move')
-
-  setTimeout(function () {
-    element.classList.remove('move')
-  }, 990)
-  setTimeout(function () {
-    element.removeChild(element.firstElementChild)
-  }, 990)
-}
 
 function setTimezone() {
     // https://stackoverflow.com/questions/9149556/how-to-get-utc-offset-in-javascript-analog-of-timezoneinfo-getutcoffset-in-c
@@ -85,5 +12,4 @@ function setTimezone() {
     tz_identifier.innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone
 }
 
-setTimezone();
-setInterval(updateTime, 100)
+setTimezone()
